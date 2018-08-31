3c124de81b51dd993150deb32a267e27360a2313
Outi Niemela <outi.niemela@hotmail.fi>
2018-08-31 23:03:06 +0300

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
-import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
+      <div id='app' className='container-fluid'>
+        <header>
+          <h1>Coloring Book</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          To get started, choose an image. (TODO: Random image, choose own image)
         </p>
       </div>
     )
